f6860cf83448a237f3a8bf6d42b846b8090fc463
ruseki <natureroseki@gmail.com>
2025-03-20 12:20:49 +0800

    updated

--- lowkey/routes/protectroutes.js
+++ lowkey/routes/protectroutes.js
@@ -1,10 +1,11 @@
-//protectroutes.js
+// protectroutes.js
 
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
-const someController = require('../controllers/someController');
 
-router.get('/protected', auth, someController.someFunction);
+router.get('/protected', auth, (req, res) => {
+    res.status(200).json({ message: 'This is a protected route!' });
+});
 
 module.exports = router;
