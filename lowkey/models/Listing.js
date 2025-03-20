ed8adb9b6bb54aa20f3a41b5a544e0df7ee6718f
ruseki <natureroseki@gmail.com>
2025-03-20 12:18:59 +0800

    updated

--- lowkey/models/Listing.js
+++ lowkey/models/Listing.js
@@ -1,96 +1,60 @@
-// Listing.js (Updated Schema)
+// Listing.js
 
 import mongoose from 'mongoose';
-
-function generateIdentifier() {
-  const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  const numbers = '0123456789';
-  let identifier = '';
-
-  for (let i = 0; i < 3; i++) {
-    identifier += letters[Math.floor(Math.random() * letters.length)];
-  }
-
-  for (let i = 0; i < 9; i++) {
-    identifier += numbers[Math.floor(Math.random() * numbers.length)];
-  }
-
-  return identifier;
-}
 
 const listingSchema = new mongoose.Schema({
   identifier: {
     type: String,
     unique: true,
-    required: true
+    required: true,
   },
   productName: {
     type: String,
-    required: true
+    required: true,
   },
   quantity: {
     type: Number,
-    required: true
+    required: true,
   },
   unit: {
     type: String,
-    required: true
+    required: true,
   },
   category: {
     type: String,
-    required: true
+    required: true,
   },
   condition: {
     type: String,
-    required: true
+    required: true,
   },
   details: {
     type: String,
-    required: true
+    required: true,
   },
   location: {
     type: String,
-    required: true
+    required: true,
   },
-  userId: {  
+  userId: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',  
-    required: true
+    ref: 'User',
+    required: true,
   },
   price: {
     type: Number,
-    required: true
+    required: true,
   },
   status: {
     type: String,
-    default: 'available' // Product status is 'available' by default
+    default: 'available', 
   },
   color: {
     type: String,
-    enum: ['red', 'green', 'blue', 'yellow', 'orange', 'purple', 'pink', 'white', 'black'], // Define colors
-    default: 'white'
-  }
+    enum: ['red', 'green', 'blue', 'yellow', 'orange', 'purple', 'pink', 'white', 'black'], //temporary
+    default: 'white',
+  },
 });
-
-listingSchema.pre('save', async function (next) {
-  if (!this.identifier) {
-    this.identifier = await genUniqIden(this.constructor);  
-  }
-  next();
-});
-
-export async function genUniqIden(Listing) {
-  let identifier;
-  let exists = true;
-
-  while (exists) {
-    identifier = generateIdentifier();
-    exists = await Listing.exists({ identifier });
-  }
-
-  return identifier;
-}
 
 const Listing = mongoose.model('Listing', listingSchema);
 export default Listing;
-
