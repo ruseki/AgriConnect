7de54851d0826d8b7a0a69c57c1eb41a7e7466eb
ruseki <natureroseki@gmail.com>
2025-04-03 18:26:30 +0800

    added the bio field

--- lowkey/models/User.js
+++ lowkey/models/User.js
@@ -11,40 +11,46 @@ function generateUserId() {
 }
 
 const UserSchema = new mongoose.Schema({
-    userId: { type: String, unique: true, required: true },
-    first_name: { type: String, required: true },
-    middle_name: { type: String },
-    last_name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    plain_text_password: { type: String, required: true },
-    otp: { type: String },
-    otpExpires: { type: Date },
-    verificationCode: { type: String },
-    isVerified: { type: Boolean, default: false },
-    userType: { type: String, default: 'user' },
-    isAdmin: { type: Boolean, default: false }, 
-    isSeller: { type: Boolean, default: false},
+  userId: { type: String, unique: true, required: true },
+  first_name: { type: String, required: true },
+  middle_name: { type: String },
+  last_name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  plain_text_password: { type: String, required: true },
+  birthDate: { type: Date, required: true },
+  country: { type: String, default: 'Philippines' },
+  province: { type: String },
+  cityOrTown: { type: String },
+  barangay: { type: String },
+  bio: { type: String }, 
+  otp: { type: String },
+  otpExpires: { type: Date },
+  verificationCode: { type: String },
+  isVerified: { type: Boolean, default: false },
+  userType: { type: String, default: 'user' },
+  isAdmin: { type: Boolean, default: false },
+  isSeller: { type: Boolean, default: false },
 }, { timestamps: true });
 
 UserSchema.pre('validate', async function (next) {
-    if (this.isNew) {
-        let uniqueId = generateUserId();
-        let existingUser = await this.constructor.findOne({ userId: uniqueId });
+  if (this.isNew) {
+    let uniqueId = generateUserId();
+    let existingUser = await this.constructor.findOne({ userId: uniqueId });
 
-        while (existingUser) {
-            uniqueId = generateUserId();
-            existingUser = await this.constructor.findOne({ userId: uniqueId });
-        }
-        this.userId = uniqueId; 
+    while (existingUser) {
+      uniqueId = generateUserId();
+      existingUser = await this.constructor.findOne({ userId: uniqueId });
     }
-    
-    if (this.isModified('password')) {
-        const hash = await bcrypt.hash(this.password, 10);
-        this.password = hash;
-    }
+    this.userId = uniqueId;
+  }
 
-    next(); 
+  if (this.isModified('password')) {
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
+  }
+
+  next();
 });
 
 module.exports = mongoose.model('User', UserSchema);
