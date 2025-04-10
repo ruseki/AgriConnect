d47dcd1f73baa48c623e05889d7c49f0c21ccdc0
Himmel221 <vincentbien187#gmail.com>
2025-04-10 19:48:44 +0800

    initial commit

--- lowkey/config/db.js
+++ lowkey/config/db.js
@@ -1,12 +1,19 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv'; // Import dotenv
+
+dotenv.config(); // Load environment variables from .env file
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+        const uri = process.env.MONGODB_URI;
+        if (!uri) {
+            throw new Error("MONGODB_URI is not defined in the .env file.");
+        }
+        await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
         console.log('MongoDB connection is successful');
     } catch (error) {
         console.error('Error connecting to MongoDB', error);
-        process.exit(1); 
+        process.exit(1);  // Exit the process with failure code
     }
 };
 
