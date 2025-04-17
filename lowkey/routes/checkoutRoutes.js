a249b97c01a5d183f5e0adf2b95b021ce1c8e73e
ruseki <natureroseki@gmail.com>
2025-04-17 15:50:51 +0800

    add route for updating buyerstatus to "received"

--- lowkey/routes/checkoutRoutes.js
+++ lowkey/routes/checkoutRoutes.js
@@ -1,7 +1,7 @@
 //checkoutRoutes.js
 
 import express from 'express';
-import { submitCheckout, getAllCheckouts, updateCheckoutStatus } from '../controllers/checkoutController.js';
+import { submitCheckout, getAllCheckouts, updateCheckoutStatus, receivedCheckout, markAsDone } from '../controllers/checkoutController.js';
 import auth from '../middleware/auth.js';
 import adminMiddleware from '../middleware/adminMiddleware.js';
 import upload from '../middleware/upload.js'; // Import the upload middleware
@@ -17,7 +17,10 @@ router.get('/all-checkouts', auth, adminMiddleware, getAllCheckouts);
 // Admin updates the status of a checkout submission
 router.patch('/:id', auth, adminMiddleware, updateCheckoutStatus);
 
-// Endpoint for submitting proof of payment
-router.post('/checkout', auth, upload.single('proofImage'), submitCheckout);
+// Buyer marks checkout as received
+router.post('/received/:id', auth, receivedCheckout);
+
+// Seller marks order as done
+router.patch('/mark-as-done/:id', auth, markAsDone);
 
 export default router;
