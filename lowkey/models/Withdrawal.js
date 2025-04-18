41843c7a846a2f9b746608ccc73ebc59321263b3
ruseki <natureroseki@gmail.com>
2025-04-18 21:58:41 +0800

    Created withdrawal schema for tracking user withdrawals

---
+++ lowkey/models/Withdrawal.js
@@ -0,0 +1,16 @@
+/* Withdrawal.js */
+
+import mongoose from 'mongoose';
+
+const WithdrawalSchema = new mongoose.Schema({
+  userId: { type: String, ref: 'User', required: true },
+  amount: { type: Number, required: true },
+  method: { type: String, required: true },
+  accountNumber: { type: String, required: true },
+  accountName: { type: String, required: true },
+  commission: { type: Number, required: true }, // 1% commission
+  date: { type: Date, default: Date.now },
+});
+
+const Withdrawal = mongoose.model('Withdrawal', WithdrawalSchema);
+export default Withdrawal;
